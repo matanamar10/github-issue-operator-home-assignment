2fadff33c7d8d70520ea7774bf4e4ac056945c50
Matan Amar <matanamar14@gmail.com>
2024-12-03 15:28:19 +0000

    Fix: Add also responses handling.

--- internal/controller/gitclient.go
+++ internal/controller/gitclient.go
@@ -2,10 +2,12 @@ package controller
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/go-github/v56/github"
+	"net/http"
 )
 
-// The IssueClient interface define interface for issuer in git: either github or gitlab.
+// The IssueClient interface defines an interface for issuers in Git, such as GitHub or GitLab.
 type IssueClient interface {
 	ListIssues(ctx context.Context, owner, repo string) ([]*github.Issue, error)
 	CreateIssue(ctx context.Context, owner, repo string, title, body string) (*github.Issue, error)
@@ -13,31 +15,67 @@ type IssueClient interface {
 	CloseIssue(ctx context.Context, owner, repo string, issueNumber int) (*github.Issue, error)
 }
 
-// GitHubIssueClient Define specific IssueClient which implemented IssueClient interface
+// GitHubIssueClient defines a specific IssueClient implementation for GitHub.
 type GitHubIssueClient struct {
 	Client *github.Client
 }
 
 func (c *GitHubIssueClient) ListIssues(ctx context.Context, owner, repo string) ([]*github.Issue, error) {
-	issues, _, err := c.Client.Issues.ListByRepo(ctx, owner, repo, nil)
-	return issues, err
+	issues, response, err := c.Client.Issues.ListByRepo(ctx, owner, repo, nil)
+	if err != nil {
+		if response != nil {
+			return nil, fmt.Errorf("failed to list issues: %s, %v", response.Status, err)
+		}
+		return nil, fmt.Errorf("failed to list issues: %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to list issues: unexpected status code %d", response.StatusCode)
+	}
+	return issues, nil
 }
 
 func (c *GitHubIssueClient) CreateIssue(ctx context.Context, owner, repo string, title, body string) (*github.Issue, error) {
 	issueRequest := &github.IssueRequest{Title: &title, Body: &body}
-	issue, _, err := c.Client.Issues.Create(ctx, owner, repo, issueRequest)
-	return issue, err
+	issue, response, err := c.Client.Issues.Create(ctx, owner, repo, issueRequest)
+	if err != nil {
+		if response != nil {
+			return nil, fmt.Errorf("failed to create issue: %s, %v", response.Status, err)
+		}
+		return nil, fmt.Errorf("failed to create issue: %v", err)
+	}
+	if response.StatusCode != http.StatusCreated && response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to create issue: unexpected status code %d", response.StatusCode)
+	}
+	return issue, nil
 }
 
 func (c *GitHubIssueClient) EditIssue(ctx context.Context, owner, repo string, issueNumber int, body string) (*github.Issue, error) {
 	editRequest := &github.IssueRequest{Body: &body}
-	issue, _, err := c.Client.Issues.Edit(ctx, owner, repo, issueNumber, editRequest)
-	return issue, err
+	issue, response, err := c.Client.Issues.Edit(ctx, owner, repo, issueNumber, editRequest)
+	if err != nil {
+		if response != nil {
+			return nil, fmt.Errorf("failed to edit issue: %s, %v", response.Status, err)
+		}
+		return nil, fmt.Errorf("failed to edit issue: %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to edit issue: unexpected status code %d", response.StatusCode)
+	}
+	return issue, nil
 }
 
 func (c *GitHubIssueClient) CloseIssue(ctx context.Context, owner, repo string, issueNumber int) (*github.Issue, error) {
 	state := "closed"
 	closeRequest := &github.IssueRequest{State: &state}
-	issue, _, err := c.Client.Issues.Edit(ctx, owner, repo, issueNumber, closeRequest)
-	return issue, err
+	issue, response, err := c.Client.Issues.Edit(ctx, owner, repo, issueNumber, closeRequest)
+	if err != nil {
+		if response != nil {
+			return nil, fmt.Errorf("failed to close issue: %s, %v", response.Status, err)
+		}
+		return nil, fmt.Errorf("failed to close issue: %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to close issue: unexpected status code %d", response.StatusCode)
+	}
+	return issue, nil
 }
