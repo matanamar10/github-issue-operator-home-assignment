4f8f90cb719ba4c8d308b23bef3f156f4f41c4e5
Matan Amar <matanamar14@gmail.com>
2024-12-04 20:57:29 +0000

    Fix: move the buissness logic to seperate helper functions for more  effective controller reconcile func.

--- internal/controller/issues.go
+++ internal/controller/issues.go
@@ -4,17 +4,19 @@ import (
 	"context"
 	"fmt"
 	"github.com/google/go-github/v56/github"
-	issues "github.com/matanamar10/github-issue-operator-hhome-assignment/api/v1alpha1"
+	issuesv1alpha1 "github.com/matanamar10/github-issue-operator-hhome-assignment/api/v1alpha1"
+	"github.com/matanamar10/github-issue-operator-hhome-assignment/internal/finalizer"
 	"go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"strings"
 	"time"
 )
 
 // searchForIssue checks if GithubIssue CRD has an issue in the repo
-func searchForIssue(issue *issues.GithubIssue, gitHubIssues []*github.Issue) *github.Issue {
+func searchForIssue(issue *issuesv1alpha1.GithubIssue, gitHubIssues []*github.Issue) *github.Issue {
 	for _, ghIssue := range gitHubIssues {
 		if ghIssue != nil && strings.EqualFold(*ghIssue.Title, issue.Spec.Title) {
 			return ghIssue
@@ -24,7 +26,7 @@ func searchForIssue(issue *issues.GithubIssue, gitHubIssues []*github.Issue) *gi
 }
 
 // UpdateIssueStatus updates the status of the GithubIssue CRD
-func (r *GithubIssueReconciler) UpdateIssueStatus(ctx context.Context, issue *issues.GithubIssue, githubIssue *github.Issue) error {
+func (r *GithubIssueReconciler) UpdateIssueStatus(ctx context.Context, issue *issuesv1alpha1.GithubIssue, githubIssue *github.Issue) error {
 	PRChange := r.CheckForPR(githubIssue, issue)
 	OpenChange := r.CheckIfOpen(githubIssue, issue)
 
@@ -50,7 +52,7 @@ func (r *GithubIssueReconciler) UpdateIssueStatus(ctx context.Context, issue *is
 }
 
 // CheckIfOpen checks if the issue is open
-func (r *GithubIssueReconciler) CheckIfOpen(githubIssue *github.Issue, issueObject *issues.GithubIssue) bool {
+func (r *GithubIssueReconciler) CheckIfOpen(githubIssue *github.Issue, issueObject *issuesv1alpha1.GithubIssue) bool {
 	if githubIssue == nil {
 		return false
 	}
@@ -77,7 +79,7 @@ func (r *GithubIssueReconciler) CheckIfOpen(githubIssue *github.Issue, issueObje
 }
 
 // CheckForPR checks if the issue has an open PR
-func (r *GithubIssueReconciler) CheckForPR(githubIssue *github.Issue, issueObject *issues.GithubIssue) bool {
+func (r *GithubIssueReconciler) CheckForPR(githubIssue *github.Issue, issueObject *issuesv1alpha1.GithubIssue) bool {
 	if githubIssue == nil {
 		return false
 	}
@@ -140,7 +142,7 @@ func (r *GithubIssueReconciler) CloseIssue(ctx context.Context, owner string, re
 }
 
 // CreateIssue creates a new issue in the repository
-func (r *GithubIssueReconciler) CreateIssue(ctx context.Context, owner string, repo string, issueObject *issues.GithubIssue) error {
+func (r *GithubIssueReconciler) CreateIssue(ctx context.Context, owner string, repo string, issueObject *issuesv1alpha1.GithubIssue) error {
 	createdIssue, err := r.IssueClient.Create(ctx, owner, repo, issueObject.Spec.Title, issueObject.Spec.Description)
 	if err != nil {
 		return fmt.Errorf("failed to create issue: %v", err)
@@ -151,7 +153,7 @@ func (r *GithubIssueReconciler) CreateIssue(ctx context.Context, owner string, r
 }
 
 // EditIssue edits the description of an existing issue in the repository
-func (r *GithubIssueReconciler) EditIssue(ctx context.Context, owner string, repo string, issueObject *issues.GithubIssue, issueNumber int) error {
+func (r *GithubIssueReconciler) EditIssue(ctx context.Context, owner string, repo string, issueObject *issuesv1alpha1.GithubIssue, issueNumber int) error {
 	issue, err := r.IssueClient.Edit(ctx, owner, repo, issueNumber, issueObject.Spec.Description)
 	if err != nil {
 		return fmt.Errorf("failed to edit issue: %v", err)
@@ -162,7 +164,7 @@ func (r *GithubIssueReconciler) EditIssue(ctx context.Context, owner string, rep
 }
 
 // FindIssue finds a specific issue in the repository by title
-func (r *GithubIssueReconciler) FindIssue(ctx context.Context, owner, repo string, issue *issues.GithubIssue) (*github.Issue, error) {
+func (r *GithubIssueReconciler) FindIssue(ctx context.Context, owner, repo string, issue *issuesv1alpha1.GithubIssue) (*github.Issue, error) {
 	allIssues, err := r.fetchAllIssues(ctx, owner, repo)
 	if err != nil {
 		return nil, fmt.Errorf("error fetching issues: %v", err)
@@ -178,4 +180,77 @@ func ParseRepoURL(repoURL string) (string, string, error) {
 		return "", "", fmt.Errorf("invalid repository URL: %s", repoURL)
 	}
 	return parts[3], parts[4], nil
+}
+
+func (r *GithubIssueReconciler) handleNewIssue(ctx context.Context, owner, repo string, issueObject *issuesv1alpha1.GithubIssue) (ctrl.Result, error) {
+	r.Log.Info("creating issue")
+	if err := r.CreateIssue(ctx, owner, repo, issueObject); err != nil {
+		r.Log.Error("failed creating issue", zap.Error(err))
+		return ctrl.Result{}, err
+	}
+
+	issue, err := r.FindIssue(ctx, owner, repo, issueObject)
+	if err != nil {
+		r.Log.Error("failed fetching newly created issue", zap.Error(err))
+		return ctrl.Result{}, err
+	}
+
+	if issueExists(issue) {
+		if err := r.UpdateIssueStatus(ctx, issueObject, issue); err != nil {
+			r.Log.Error("failed updating issue status", zap.Error(err))
+		}
+	} else {
+		r.Log.Warn("Cannot update status: issue is nil", zap.String("IssueName", issueObject.Name), zap.String("Namespace", issueObject.Namespace))
+	}
+
+	r.Log.Info("issue created successfully")
+	return ctrl.Result{}, nil
+}
+
+func (r *GithubIssueReconciler) handleUpdatedIssue(ctx context.Context, owner, repo string, issueObject *issuesv1alpha1.GithubIssue, issue *github.Issue) (ctrl.Result, error) {
+	r.Log.Info("editing issue")
+	if err := r.EditIssue(ctx, owner, repo, issueObject, *issue.Number); err != nil {
+		r.Log.Error("failed editing issue", zap.Error(err))
+		return ctrl.Result{}, err
+	}
+
+	updatedIssue, err := r.FindIssue(ctx, owner, repo, issueObject)
+	if err != nil {
+		r.Log.Error("failed fetching updated issue", zap.Error(err))
+		return ctrl.Result{}, err
+	}
+
+	if issueExists(updatedIssue) {
+		if err := r.UpdateIssueStatus(ctx, issueObject, updatedIssue); err != nil {
+			r.Log.Error("failed updating issue status", zap.Error(err))
+		}
+	} else {
+		r.Log.Warn("Cannot update status: issue is nil", zap.String("IssueName", issueObject.Name), zap.String("Namespace", issueObject.Namespace))
+	}
+
+	r.Log.Info("issue edited successfully")
+	return ctrl.Result{}, nil
+}
+
+func (r *GithubIssueReconciler) handleDeletion(ctx context.Context, owner, repo string, issue *github.Issue, issueObject *issuesv1alpha1.GithubIssue) (ctrl.Result, error) {
+	r.Log.Info("closing issue")
+	if !issueExists(issue) {
+		return ctrl.Result{}, fmt.Errorf("cannot close issue: issue is nil")
+	}
+
+	if err := r.CloseIssue(ctx, owner, repo, issue); err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed closing issue: %v", err)
+	}
+
+	if err := finalizer.Cleanup(ctx, r.Client, issueObject, r.Log); err != nil {
+		r.Log.Error("failed cleaning up finalizer", zap.Error(err))
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+// Helper function to check if an issue exists.
+func issueExists(issue *github.Issue) bool {
+	return issue != nil
 }
